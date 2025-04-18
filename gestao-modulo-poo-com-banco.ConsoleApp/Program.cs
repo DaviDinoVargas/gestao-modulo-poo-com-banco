c76f59f2fac52ae4c874ddc63235e4397d5b1112
davidinovargas <davidinovargas@gmail.com>
2025-04-18 19:54:19 -0300

    Add project files.

---
+++ gestao-modulo-poo-com-banco.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace gestao_modulo_poo_com_banco.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
